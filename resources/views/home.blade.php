f40a77d2637afb8ddd4eb719d636d5c105f6279b
Elzie Crous <elsabe83@gmail.com>
2018-09-28 18:16:53 -0500

    added styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div id="root"></div>
-    <hr>
-    <h2>Following</h2>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-2">
+            <img src="{{ Auth::user()->avatar }}" alt="{{ Auth::user()->username }}" height="80">
+            <hr>
+                <h2 class="text-primary">{{ Auth::user()->username }}</h2>
+            <div class="col-md-2">
+   
+    <h3>Following</h3>
 
     @foreach($following as $user)
-        <a href="{{ route('users', $user) }}">{{ $user->username }}</a>
+        <a href="{{ route('users', $user) }}" class="btn btn-primary" >{{ $user->username }}</a>
     @endforeach
 
 
 <hr>
-    <h2>Followers</h2>
+    <h3>Followers</h3>
 
     @foreach($followers as $user)
-        <a href="{{ route('users', $user) }}">{{ $user->username }}</a>
+        <a href="{{ route('users', $user) }}" class="btn btn-success">{{ $user->username }}</a>
     @endforeach
-</div>
+    </div>
+        </div>
+    <div class="col-md-10">
+    <div id="root"></div>
+    </div>
+    </div>
+
 </div>
 @endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 <div class="container">
-    {{$user->username}}
+    <h2 class="text-primary">{{$user->username}}</h2>
 <hr>
     @if(Auth::user()->isNotTheUser($user))
         @if(Auth::user()->isFollowing($user))
-            <p><a href="{{ route('users.unfollow', $user) }}">Unfollow</a></p>
+            <p><a href="{{ route('users.unfollow', $user) }}" class="btn btn-primary" >Unfollow</a></p>
         @else
-        <p><a href="{{ route('users.follow', $user) }}">Follow</a></p>
+        <p><a href="{{ route('users.follow', $user) }}" class="btn btn-success">Follow</a></p>
         @endif
 
     @endif   
